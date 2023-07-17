8d94819881281a333c07eb1412470a9647075f16
Kushagrasingh007 <136775222+Kushagrasingh007@users.noreply.github.com>
2023-07-16 23:47:39 -0400

    Update server.js
    version 18

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! By Kushagra Singh, Version 17\n');
+  res.end('Hello, World! By Kushagra Singh, Version 18\n');
 });
 
 const port = 3000;
